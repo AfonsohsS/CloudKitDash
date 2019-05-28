611d326cf30ad10a4bfff062288b70811bed1c9c
AfonsohsS <afonsohs@gmail.com>
2019-05-27 22:10:34 -0300

    Implemented Contact the CloudKit Servers with Subscription (Operations and FetchOerations)

--- CloudKitDash/AppDelegate.swift
+++ CloudKitDash/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CloudKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,32 +16,52 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        /*
+        Note: Subscriptions only report changes in customs zones.
+         Therefore, if we want to receive notifications, besidescreating the
+         subscription we also have to create a record zone and store all our
+         records in it. This is why, the first thing we do when the application
+         is launched, is to store two Boolean values in the User Defaults database
+         called "subscriptionSaved" and "zoneCreated". These values will be used
+         later to know whether or not we have already created the subscription and
+         the custom zone.
+        */
+        let userSettings = UserDefaults.standard
+        let values = ["subscriptionSaved" : false, "zoneCreated" : false]
+        userSettings.register(defaults: values)
+        
+        //Register the application with icloud servers
+        application.registerForRemoteNotifications()
+        
+        AppData.configureDatabase {}
+        
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        
+        //Check whether the notification received is of type CKDatabaseNotification, sent by a CloudKit server,
+        //creating a CKNotification
+        let notification = CKNotification(fromRemoteNotificationDictionary: userInfo) as? CKDatabaseNotification
+        
+        /*
+         Note: We call the closure to tell the system that the process is over,
+         but because the operations are performed asynchronously, we can't do it
+         until they are finished. This way we send a closure to the method we use
+         to download the new information which sole purpose is to execute the
+         completionHandler closure with the value "result" returned by the operations.
+         */
+        
+        if notification != nil {
+            AppData.checkUpdates { (result) in
+                let mainQueue = OperationQueue.main
+                mainQueue.addOperation {
+                    //Tell to excute closure only with process is over.
+                    //Por isso usamos o result na sua execução.
+                    completionHandler(result)
+                }
+            }
+        }
     }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
 
--- CloudKitDash/Models/ApplicationData.swift
+++ CloudKitDash/Models/ApplicationData.swift
@@ -40,8 +40,12 @@ class ApplicationData {
         
         if text != "" {
             
-            //Create a unique value ID for the Countries
-            let id = CKRecord.ID(recordName: "idcountry-\(UUID())")
+            //As we know subscriptions require the records to be stored in a custom zone.
+            // So we have to create one.
+            let zone = CKRecordZone(zoneName: "listPlaces")
+            
+            //Create a unique value ID for the Countries in a specific custom zone
+            let id = CKRecord.ID(recordName: "idcountry-\(UUID())", zoneID: zone.zoneID)
             
             //Create a record object of type Countries
             let record = CKRecord(recordType: "Countries", recordID: id)
@@ -71,8 +75,12 @@ class ApplicationData {
         
         if text != "" {
             
-            //Create a unique value ID for the Countries
-            let id = CKRecord.ID(recordName: "idcity-\(UUID())")
+            //As we know subscriptions require the records to be stored in a custom zone.
+            // So we have to create one.
+            let zone = CKRecordZone(zoneName: "listPlaces")
+            
+            //Create a unique value ID for the City in a specific custom zone
+            let id = CKRecord.ID(recordName: "idcity-\(UUID())", zoneID: zone.zoneID)
             
             //Create a record object of type Cities
             let record = CKRecord(recordType: "Cities", recordID: id)
@@ -162,8 +170,7 @@ class ApplicationData {
     
     //MARK: - Update Interface
     
-    //The purpose of this method is to tell the rest of the application
-    //that new information is available and should be shown to the user.
+    ///The purpose of this method is to tell the rest of the application that new information is available and should be shown to the user.
     func updateInterface() {
         let main = OperationQueue.main
         main.addOperation {
@@ -173,7 +180,7 @@ class ApplicationData {
         }
     }
     
-    //Alert Template
+    ///Alert Template for Country and City
     func setAlert(type: String, title: String, style: UIAlertController.Style, message: String?) -> UIAlertController {
         
         let alert = UIAlertController(title: title, message: message, preferredStyle: style)
@@ -196,6 +203,301 @@ class ApplicationData {
         alert.addTextField(configurationHandler: nil)
         
         return alert
+    }
+    
+    //MARK: - Contact the CloudKit servers
+    //Two methods that are going to contact the CloudKit servers and process the information by Subscription
+    
+    ///Method to create the subscription and the zone
+    func configureDatabase(executeClosure: @escaping () -> Void) {
+        let userSettings = UserDefaults.standard
+        
+        //Check whether subscriptionSaved has been created
+        if !userSettings.bool(forKey: "subscriptionSaved") {
+            
+            //If not we create a new CKDatabaseSubscription
+            let newSubscription = CKDatabaseSubscription(subscriptionID: "updatesDatabase")
+            
+            //Setting the Nofication are going to be sent by the server
+            let info = CKSubscription.NotificationInfo()
+            info.shouldSendContentAvailable = true
+            newSubscription.notificationInfo = info
+            
+            //The subscription is saved on the server
+            database.save(newSubscription) { (subscription, error) in
+                
+                if error != nil {
+                    print("ERROR Creating Subscription")
+                } else {
+                    
+                    //Set TRUE if the operation is successful
+                    userSettings.set(true, forKey: "subscriptionSaved")
+                }
+            }
+        }
+        
+        //Create the custom Zone and check if it was already created
+        if !userSettings.bool(forKey: "zoneCreated") {
+            
+            //Create Zone listPlaces to store our records
+            let newZone = CKRecordZone(zoneName: "listPlaces")
+            
+            //The newZone is saved on the server
+            database.save(newZone) { (zone, error) in
+                
+                if error != nil {
+                    print("ERROR Creating Zone")
+                } else {
+                    
+                    //Set TRUE if the operation is successful
+                    userSettings.set(true, forKey: "zoneCreated")
+                    
+                    //We call the closure because we have to be sure that the zone
+                    //was created before trying to store any records.
+                    executeClosure()
+                }
+            }
+        } else {
+            
+            //The closure is also called if the zone was already created to make sure
+            //that its code is always executed.
+            executeClosure()
+        }
+    }
+    
+    ///Method to download and process the changes in the database
+    func checkUpdates(finishClosure: @escaping (UIBackgroundFetchResult) -> Void) {
+        
+        //To make sure that the database is configured properly, we call configureDatabase()
+        configureDatabase {
+            
+            //Code to be execute after we make sure that the Zone was created
+            let mainQueue = OperationQueue.main
+            mainQueue.addOperation {
+                self.downloadUpdates(finishClosure: finishClosure)
+            }
+        }
+    }
+    
+    ///Getting the updates from the server
+    func downloadUpdates(finishClosure: @escaping (UIBackgroundFetchResult) -> Void) {
+        
+        // 1. Defining the properties we are going to use to store the tokens
+        // (one for the database and another for the custom zone)
+        
+        //For the database Token
+        var changeToken: CKServerChangeToken!
+        
+        //For the our custom Zone
+        var changeZoneToken: CKServerChangeToken!
+        
+        let userSettings = UserDefaults.standard
+        
+        // 2. Check if there are tokens already stored in the User Defaults Database
+        
+        //Note: Because the tokens are instances of the CKServerChangeToken class,
+        // we can't store their values directly in User Defaults, we first have
+        // to convert them into Data structures.
+        
+        if let data = userSettings.value(forKey: "changeToken") as? Data {
+            
+            //If there is a token unarchive it...
+            if let token = try? NSKeyedUnarchiver.unarchivedObject(ofClass: CKServerChangeToken.self, from: data) {
+                
+                // and store it here.
+                changeToken = token
+            }
+        }
+        
+        if let data = userSettings.value(forKey: "changeZoneToken") as? Data {
+            
+            //If there is a token unarchive it...
+            if let token = try? NSKeyedUnarchiver.unarchivedObject(ofClass: CKServerChangeToken.self, from: data) {
+                
+                // and store it here.
+                changeZoneToken = token
+            }
+        }
+        
+        // 3. Configure the operations necessary to get the updates from the server.
+        
+        //Note: We have to perform two operations on the database, one to download
+        // the list of changes available and another to download the actual changes
+        // and show them to the user
+        
+        var zonesIDs: [CKRecordZone.ID] = []
+        
+        //This Operation requires the previous database token to get only the changes
+        // that are not available on the device, so we pass the value of the changeToken property.
+        let operation = CKFetchDatabaseChangesOperation(previousServerChangeToken: changeToken)
+        
+        //Report the zone that changed
+        // The closure assigned to this property is executed every time the system
+        // finds a zone whose content has changed.
+        operation.recordZoneWithIDChangedBlock = { (zoneID) in
+            zonesIDs.append(zoneID)
+        }
+        
+        //Report the creation a new database token
+        // This closure is executed every time the system decides to perform the operation
+        // again to download the changes in separate processes. To make sure that we only
+        // receive the changes that we did not process yet, we use this closure to update
+        // the changeToken property with the current token.
+        operation.changeTokenUpdatedBlock = { (token) in
+            changeToken = token
+        }
+        
+        //Report the conclusion of the operation
+        // The closure assigned to this property is executed to let the app know that the
+        // operation is over, and this is the signal that indicates that we have all the
+        // information we need to begin downloading the changes with the second operation.
+        
+        //Note: This last closure receives three values: the latest database token, a Boolean
+        // value that indicates whether there are "more changes" available (by default, the system
+        // takes care of fetching all the changes, so we don't need to consider this value),
+        // and a CKError structure to report errors
+        operation.fetchDatabaseChangesCompletionBlock = { (token, more, error) in
+            if error != nil {
+                finishClosure(.failed)
+                print("ERROR in fetchDatabseChangesComplitionBlock")
+                
+                
+            } else if !zonesIDs.isEmpty {
+                
+                //If the array is not empty, we have to configure the CKFetchRecordZoneChangesOperation
+                // operation to get the changes.
+                changeToken = token
+                
+                //Because in this example we only work with one zone, we read the first element of the
+                // zonesIDs array to get the ID of our custom zone and provide a ZoneConfiguration object
+                // with the current token stored in the changeZoneToken property.
+                let configuration = CKFetchRecordZoneChangesOperation.ZoneConfiguration()
+                configuration.previousServerChangeToken = changeZoneToken
+                
+                let fetchOperation = CKFetchRecordZoneChangesOperation(recordZoneIDs: zonesIDs, configurationsByRecordZoneID: [zonesIDs[0] : configuration])
+                
+                //The changes are fetched, and the results are reported to the closures assigned to its properties.
+                
+                //Operation 1: This closure is called every time a new or updated record is received.
+                fetchOperation.recordChangedBlock = { (record) in
+                    
+                    //First we have to check if the record is of type Countries or Cities
+                    // and store the record in the corresponding array
+                    if record.recordType == "Countries" {
+                        
+                        //We use the firstIndex() method to look for duplicates
+                        //If the record already exists in the array, we update its values,
+                        // otherwise, we add the record to the list.
+                        let index = self.listCountries.firstIndex(where: { (item) -> Bool in
+                            return item.recordID == record.recordID
+                        })
+                        if index != nil {
+                            self.listCountries[index!] = record
+                        } else {
+                            self.listCountries.append(record)
+                        }
+                        
+                    } else if record.recordType == "Cities" {
+                        
+                        //we first check whether the record contains a reference to a country and
+                        //only update or add the record to the array if the reference corresponds
+                        //to the country currently selected by the user
+                        if let country = record["country"] as? CKRecord.Reference {
+                            if country.recordID == self.selectedCountry {
+                                
+                                //We use the firstIndex() method to look for duplicates.
+                                //If the record already exists in the array, we update its values,
+                                // otherwise, we add the record to the list.
+                                let index = self.listCities.firstIndex(where: { (item) -> Bool in
+                                    return item.recordID == record.recordID
+                                })
+                                if index != nil {
+                                    self.listCities[index!] = record
+                                } else {
+                                    self.listCities.append(record)
+                                }
+                            }
+                        }
+                    }
+                }
+                
+                //Operation 2: This closure is called every time the app receives the ID of a deleted
+                //record (a record that was deleted from the CloudKit database).
+                fetchOperation.recordWithIDWasDeletedBlock = { (recordID, recordType) in
+                    if recordType == "Countries" {
+                        let index = self.listCountries.firstIndex(where: { (item) -> Bool in
+                            return item.recordID == recordID
+                        })
+                        if index != nil {
+                            self.listCountries.remove(at: index!)
+                        }
+                    } else if recordType == "Cities" {
+                        let index = self.listCities.firstIndex(where: { (item) -> Bool in
+                            return item.recordID == recordID
+                        })
+                        if index != nil {
+                            self.listCities.remove(at: index!)
+                        }
+                    }
+                }
+                
+                //Operation 3: Next two closures are executed when the process completes a cycle
+                //Note: Depending on the characteristics of our application, we may need to perform
+                // some tasks in these closures
+                fetchOperation.recordZoneChangeTokensUpdatedBlock = { (zoneID, token, data) in
+                    changeZoneToken = token
+                }
+                
+                //Operation 4: The same above
+                fetchOperation.recordZoneFetchCompletionBlock = { (zoneID, token, data, more, error) in
+                    if error != nil {
+                        print("Error in fetchOperation.recordZoneFetchCompletionBlock")
+                    } else {
+                        changeZoneToken = token
+                    }
+                }
+                
+                //Operation 5: This closure is called to report that the operation is over.
+                fetchOperation.fetchRecordZoneChangesCompletionBlock = { (error) in
+                    if error != nil {
+                        finishClosure(.failed)
+                        print("Error in fetchOperation.fetchRecordZoneChangesCompletionBlock")
+                    } else {
+                        
+                        //If no error is found, we permanently store the current tokens in the User Defaults database
+                        if changeToken != nil {
+                            
+                            //To store the tokens we have to turn them into Data structures and encode them with the archivedData()
+                            if let data = try? NSKeyedArchiver.archivedData(withRootObject: changeToken!, requiringSecureCoding: false) {
+                                userSettings.set(data, forKey: "changeToken")
+                            }
+                        }
+                        if changeZoneToken != nil {
+                            
+                            //To store the tokens we have to turn them into Data structures and encode them with the archivedData()
+                            if let data = try? NSKeyedArchiver.archivedData(withRootObject: changeZoneToken!, requiringSecureCoding: false) {
+                                userSettings.set(data, forKey: "changeZoneToken")
+                            }
+                        }
+                        self.updateInterface()
+                        //If there are chnages we have to tell the system that new data has been downloaded.
+                        finishClosure(.newData)
+                    }
+                }
+                
+                // 4. After the definition of each fetchOperation and their properties, we call the add() method
+                // of the CKDatabase object to add them to the database.
+                self.database.add(fetchOperation)
+            } else {
+                
+                //If there are no changes available
+                finishClosure(.noData)
+            }
+        }
+        
+        // 5. After the definition of each operation and their properties, we call the add() method
+        // of the CKDatabase object to add them to the database.
+        database.add(operation)
     }
 }
 
